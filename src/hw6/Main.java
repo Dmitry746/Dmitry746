8fe5e10cf07eba0f3a6eac7ce315b6c9bb1c0ffa
Dmitry746 <chugunov06@mail.ru>
2021-08-05 15:53:10 +0300

    Домашка.№6

--- src/hw6/Main.java
+++ src/hw6/Main.java
@@ -1,2 +1,9 @@
-package hw6;public class Main {
+package hw6;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
 }
